fee8615efbd1f5ca77ce149a362ad5f93836f4b1
TechGyal <jadah@vt.edu>
2025-07-23 18:35:29 -0400

    add spring boot project

---
+++ src/main/java/com/example/greggoAPI/GreggoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.greggoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreggoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreggoApiApplication.class, args);
+	}
+
+}
