26c52c1acfda6b1ebeb679fa020f1ab70f569ccd
TechGyal <jadah@vt.edu>
2025-07-24 13:06:21 -0400

    added photo and description attribute to greg user

--- src/main/java/com/example/greggoAPI/GregUser.java
+++ src/main/java/com/example/greggoAPI/GregUser.java
@@ -3,10 +3,14 @@ package com.example.greggoAPI;
 public class GregUser implements User {
     private int uniqueID;
     private Location location;
+    private String description;
+    private String photoFilePath; // File path for photo
 
-    public GregUser(int uniqueID, Location location) {
+    public GregUser(int uniqueID, Location location, String description, String photoFilePath) {
         this.uniqueID = uniqueID;
         this.location = location;
+        this.description = description;
+        this.photoFilePath = photoFilePath;
     }
 
     @Override
@@ -18,12 +22,21 @@ public class GregUser implements User {
         return location;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPhotoFilePath() {
+        return photoFilePath;
+    }
 
     @Override
     public String toString() {
         return "GregUser{" +
                 "uniqueID=" + uniqueID +
                 ", location=" + location +
+                ", description='" + description + '\'' +
+                ", photoFilePath='" + photoFilePath + '\'' +
                 '}';
     }
 }
